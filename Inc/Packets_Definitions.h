f560d13794651f0ada5341b588313cde334a5618
Angus McLennan <amcl0006@student.monash.edu>
2024-01-16 19:39:59 +1100

    Squashed 'libs/Strelka_Ground_Station_Packets/' changes from 987c8e6..cb7fdab
    cb7fdab Added streaming packets to header file
    d3d4e1b Added stream t0 pkt and protocol version metadata
    
    git-subtree-dir: libs/Strelka_Ground_Station_Packets
    git-subtree-split: cb7fdab60420463f0d8f761c7c96b3517f5e6527

--- Inc/Packets_Definitions.h
+++ Inc/Packets_Definitions.h
@@ -45,6 +45,15 @@
 #define GPS_TRACKING_CONFIG_RES
 #define GPS_TRACKING_CONFIG_SET
 #define GPS_TRACKING_PACKET
+#define STREAM_PKT_CONFIG
+#define STREAM_PACKET_TYPE_0
+#define STREAM_PACKET_TYPE_1
+#define STREAM_PACKET_TYPE_2
+#define STREAM_PACKET_TYPE_3
+#define STREAM_PACKET_TYPE_4
+#define STREAM_PACKET_TYPE_5
+#define STREAM_PACKET_TYPE_6
+#define STREAM_PACKET_TYPE_7
 
 /* Payload lengths */
 #define BAT_VOL_REQ_PKT_LEN
@@ -81,6 +90,15 @@
 #define GPS_TRACKING_CONFIG_RES_PKT_LEN
 #define GPS_TRACKING_CONFIG_SET_PKT_LEN
 #define GPS_TRACKING_PACKET_PKT_LEN
+#define STREAM_PKT_CONFIG_LEN
+#define STREAM_PACKET_TYPE_0_LEN
+#define STREAM_PACKET_TYPE_1_LEN
+#define STREAM_PACKET_TYPE_2_LEN
+#define STREAM_PACKET_TYPE_3_LEN
+#define STREAM_PACKET_TYPE_4_LEN
+#define STREAM_PACKET_TYPE_5_LEN
+#define STREAM_PACKET_TYPE_6_LEN
+#define STREAM_PACKET_TYPE_7_LEN
 
 typedef struct
 {
@@ -209,5 +227,85 @@ typedef struct
 	float altitude;
 	uint8_t satellites_tracked;
 } gps_tracking_packet;
+
+typedef struct 
+{
+	uint8_t packet_type_0_enable;
+	float packet_type_0_stream_frequency;
+	uint8_t packet_type_1_enable;
+	float packet_type_1_stream_frequency;
+	uint8_t packet_type_2_enable;
+	float packet_type_2_stream_frequency;
+	uint8_t packet_type_3_enable;
+	float packet_type_3_stream_frequency;
+	uint8_t packet_type_4_enable;
+	float packet_type_4_stream_frequency;
+	uint8_t packet_type_5_enable;
+	float packet_type_5_stream_frequency;
+	uint8_t packet_type_6_enable;
+	float packet_type_6_stream_frequency;
+	uint8_t packet_type_7_enable;
+	float packet_type_7_stream_frequency;
+} stream_packet_config;
+
+typedef struct 
+{
+	uint32_t timestamp;
+	float latitude;
+	float longitude;
+	float gps_altitude;
+	uint8_t satellites_tracked;
+	float lin_accX;
+	float lin_accY;
+	float lin_accZ;
+	float lin_velX;
+	float lin_velY;
+	float lin_velX;
+	float baro_altitude;
+	float ang_velX;
+	float ang_velY;
+	float ang_velZ;
+	float quaternion_q1;
+	float quaternion_q2;
+	float quaternion_q3;
+	float quaternion_q4;
+	float battery_voltage;
+	uint8_t flight_state;
+} stream_packet_type_0;
+
+typedef struct 
+{
+
+} stream_packet_type_1;
+
+typedef struct 
+{
+
+} stream_packet_type_2;
+
+typedef struct 
+{
+
+} stream_packet_type_3;
+
+typedef struct 
+{
+
+} stream_packet_type_4;
+
+typedef struct 
+{
+
+} stream_packet_type_5;
+
+typedef struct 
+{
+
+} stream_packet_type_6;
+
+typedef struct 
+{
+
+} stream_packet_type_7;
 
 #endif /* INC_PACKETS_H_ */
