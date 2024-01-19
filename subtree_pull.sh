3d7ad41fe0a155c6bdf4f565f132e4b724539d3b
Angus McLennan <amcl0006@student.monash.edu>
2024-01-19 18:32:16 +1100

    Added submodules for chirp

--- subtree_pull.sh
+++ subtree_pull.sh
@@ -5,11 +5,11 @@ git fetch git@github.com:Hover-Disaster/STM32_MS5611.git
 git fetch git@github.com:Hover-Disaster/STM32_SD.git 
 git fetch git@github.com:Hover-Disaster/STM32_LoRa.git 
 git fetch git@github.com:Hover-Disaster/Strelka_State_Machine.git
-git fetch git@github.com:Hover-Disaster/Strelka_Ground_Station_Packets.git
+git fetch git@github.com:s-park21/CHIRP.git
 
 git subtree pull --prefix=libs/STM32_BMX055 git@github.com:Hover-Disaster/STM32_BMX055.git main --squash
 git subtree pull --prefix=libs/STM32_MS5611 git@github.com:Hover-Disaster/STM32_MS5611.git main --squash
 git subtree pull --prefix=libs/STM32_SD git@github.com:Hover-Disaster/STM32_SD.git main --squash
 git subtree pull --prefix=libs/STM32_SD git@github.com:Hover-Disaster/STM32_LoRa.git main --squash
 git subtree pull --prefix=libs/state_machine git@github.com:Hover-Disaster/Strelka_State_Machine.git main --squash
-git subtree pull --prefix=libs/Strelka_Ground_Station_Packets git@github.com:Hover-Disaster/Strelka_Ground_Station_Packets.git main --squash
+git subtree pull --prefix=libs/CHIRP git@github.com:s-park21/CHIRP.git main --squash
--- subtree_push.sh
+++ subtree_push.sh
@@ -7,12 +7,11 @@ git fetch git@github.com:Hover-Disaster/STM32_MS5611.git
 git fetch git@github.com:Hover-Disaster/STM32_SD.git 
 git fetch git@github.com:Hover-Disaster/STM32_LoRa.git 
 git fetch git@github.com:Hover-Disaster/Strelka_State_Machine.git
-git fetch git@github.com:Hover-Disaster/Strelka_Ground_Station_Packets.git
-
+git fetch git@github.com:s-park21/CHIRP.git
 
 git subtree push --prefix=libs/STM32_BMX055 git@github.com:Hover-Disaster/STM32_BMX055.git main
 git subtree push --prefix=libs/STM32_MS5611 git@github.com:Hover-Disaster/STM32_MS5611.git main
 git subtree push --prefix=libs/STM32_SD git@github.com:Hover-Disaster/STM32_SD.git main
 git subtree push --prefix=libs/STM32_SD git@github.com:Hover-Disaster/STM32_LoRa.git main
 git subtree push --prefix=libs/state_machine git@github.com:Hover-Disaster/Strelka_State_Machine.git main
-git subtree push --prefix=libs/Strelka_Ground_Station_Packets git@github.com:Hover-Disaster/Strelka_Ground_Station_Packets.git main
+git subtree push --prefix=libs/CHIRP git@github.com:s-park21/CHIRP.git main
