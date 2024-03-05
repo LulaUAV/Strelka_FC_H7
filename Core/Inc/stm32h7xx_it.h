54806c5a341f31712347498f6f82b5411cd08246
Angus McLennan <amcl0006@student.monash.edu>
2024-03-05 21:43:39 +1100

    Added test CANFD code

--- Core/Inc/stm32h7xx_it.h
+++ Core/Inc/stm32h7xx_it.h
@@ -57,7 +57,6 @@ void EXTI3_IRQHandler(void);
 void EXTI4_IRQHandler(void);
 void DMA1_Stream0_IRQHandler(void);
 void FDCAN1_IT0_IRQHandler(void);
-void FDCAN1_IT1_IRQHandler(void);
 void EXTI9_5_IRQHandler(void);
 void TIM2_IRQHandler(void);
 void USART2_IRQHandler(void);
